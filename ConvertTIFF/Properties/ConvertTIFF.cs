16f14d68045fa6a64f71eb4e08b92b77ccb1d693
wither <wither@localhost>
2022-09-19 13:49:57 -0700

    Initial commit

---
+++ ConvertTIFF/Properties/ConvertTIFF.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+
+namespace ConvertTIFF.Properties;
+
+public static class ConvertTIFF
+{
+    [FunctionName("ConvertTIFF")]
+    public static async Task RunAsync(
+        [BlobTrigger("samples-workitems/{name}.tif{extension}", Connection = "")] Stream myBlob,
+        [Blob("samples-workitems", FileAccess.Write)] BlobContainerClient outputImage, string name, string extension,
+        ILogger log)
+    {
+        // Create one stream per TIFF Page
+        MemoryStream[] outputFile = Split(myBlob);
+        int i = 0;
+        foreach (MemoryStream stream in outputFile)
+        {
+            stream.Position = 0;
+            // Upload PNGs to Blob Storage
+            outputImage.UploadBlob($"{name}-p{i}.png", stream);
+            i++;
+        }
+    }
+
+    private static MemoryStream[] Split(Stream inputFile)
+    {
+        // Get the frame dimension list from the image of the file and 
+        Image tiffImage = Image.FromStream(inputFile);
+        // Get the globally unique identifier (GUID) 
+        Guid objGuid = tiffImage.FrameDimensionsList[0];
+        // Create the frame dimension 
+        FrameDimension dimension = new FrameDimension(objGuid);
+        // Gets the total number of frames in the .tiff file 
+        int noOfPages = tiffImage.GetFrameCount(dimension);
+
+        // Check for existence of TIFF Decoder
+        ImageCodecInfo[] imageEncoders = ImageCodecInfo.GetImageEncoders();
+        ImageCodecInfo encodeInfo = imageEncoders.FirstOrDefault(t => t.MimeType == "image/tiff");
+        var outputFile = new MemoryStream[noOfPages];
+        foreach (Guid guid in tiffImage.FrameDimensionsList)
+        {
+            for (int index = 0; index < noOfPages; index++)
+            {
+                FrameDimension currentFrame = new FrameDimension(guid);
+                tiffImage.SelectActiveFrame(currentFrame, index);
+                // Save as PNG
+                outputFile[index] = new MemoryStream();
+                tiffImage.Save(outputFile[index], ImageFormat.Png);
+            }
+        }
+
+        return outputFile;
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Convert TIFF
+
+Convert TIFF is an Azure Function that monitors blob storage for new TIFF
+files, and converts multi-page TIFFs into multiple PNG files.
+
+
+
